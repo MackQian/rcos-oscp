e7ca8f9a8236391402d45507a11a67af69a840c5
MackQian <37890170+MackQian@users.noreply.github.com>
2018-12-11 21:48:25 -0500

    Add files via upload

---
+++ venvs/curriculumProjectVenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dankstar/Dropbox/RCOS/FALL2018/venvs/curriculumProjectVenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
